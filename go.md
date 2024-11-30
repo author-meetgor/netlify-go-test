1a1f976f58643263c72ed98c1495724395fbba17
author-meetgor <words.meetgor@gmail.com>
2024-11-30 16:24:50 +0530

    fix

--- go.md
+++ go.md
@@ -1,3 +1,3 @@
 module github.com/author-meetgor/netlify-golang-test
 
-go 1.23.3
+go 1.19
--- netlify/functions/hello/main.go
+++ netlify/functions/hello/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
+  "github.com/aws/aws-lambda-go/events"
+  "github.com/aws/aws-lambda-go/lambda"
 )
 
-func Handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-    return &events.APIGatewayProxyResponse{
-        StatusCode: 200,
-        Body: "Hello, Gophers!",
-    }, nil
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+  return &events.APIGatewayProxyResponse{
+    StatusCode:        200,
+    Body:              "Hello, Gophers!",
+  }, nil
 }
 
 func main() {
-    lambda.Start(Handler)
+  lambda.Start(handler)
 }
